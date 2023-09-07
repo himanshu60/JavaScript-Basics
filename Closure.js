c02f1b9dcb0fe9475c9de9562bf258ec524aa963
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-09-07 10:43:58 +0530

    closure in js

--- Closure.js
+++ Closure.js
@@ -1,7 +1,7 @@
 // In Js closure is a concept that describe how functions remember and can access variables from their containing or outer scopes, even after the outer function has finished executing.
 // closure allow you to create and maintain private data within functions and are a fundamental part of Javascript behaviour.
 
-
+// Example-1
 function outerFunc(){
     // This variable is part of the outer function's scope.
     var outVariable="I am from outer function";
@@ -17,3 +17,25 @@ function outerFunc(){
 var closure=outerFunc();
 // Now, you can use the closure to access outerVariable even though outerFunction has finished executing.
 closure();// Outputs: "I am from the outer function"
+
+
+// Example2
+
+function counter(){
+    let count=0;
+    function increment(){
+        count++;
+        console.log(count);
+    }
+    return increment;
+}
+
+var res=counter();
+var ans=counter();
+
+res();//1
+res();//2
+
+ans();//1
+res();//3
+ans();//2
