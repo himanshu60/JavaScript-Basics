b0f7507bbb4c67c934fdbea0dca1d4bddb50c94a
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-09-07 11:48:41 +0530

    event loop in js

---
+++ Eventloop.js
@@ -0,0 +1,15 @@
+// In JS , the event loop is a fundamental concept that allows JS to manage asynchronous operation.
+// It is the mechanism that controls the execution of code in a non-blocking way, ensure that your program can continue run smoothly.
+
+// Example
+
+console.log(`Start the Program`);
+// This function simulates an asynchronous task (like reading a file or making an HTTP request).
+function asyncTask(){
+    setTimeout(()=>{
+        console.log(`async task is done...`);
+    },2000);
+}
+
+asyncTask();
+console.log(`End the Program`);
