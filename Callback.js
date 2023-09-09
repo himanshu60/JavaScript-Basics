e23056a24e9c14e9c5e6821dc8ce433f40555dca
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-09-09 16:34:31 +0530

    callback in js

--- Callback.js
+++ Callback.js
@@ -1,6 +1,9 @@
-// In JS , a callback is a function that passed as an argument to another function and is executed after the completion of the function.
+// In JS , a callback is a function that passed as an argument to another function and is executed 
+// after the completion of the function.
 
-// Callbacks are often used for asynchronous operation,such as reading file, making networking request, or handle user interations, where we dont know exactly the operation will finish, but we want to perform some action when it does.
+// Callbacks are often used for asynchronous operation,such as reading file, making networking request, 
+// or handle user interations, where we dont know exactly the operation will finish, 
+// but we want to perform some action when it does.
 
 
 // Example
