9640117ca30ae9db808b4a8bf947bd0a72a1f85e
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-09-28 18:28:34 +0530

    class

--- Class.js
+++ Class.js
@@ -25,3 +25,29 @@ const person1 = new Person("Himanshu", "Choudhary", 30);
 console.log(person1.firstName); // Output: "Himanshu"
 console.log(person1.fullName()); // Output: "Himanshu Choudhary"
 console.log(person1.isAdult()); // Output: true
+
+// Example 2
+
+class Car {
+  constructor(make, type, year) {
+    this.make = make;
+    this.type = type;
+    this.year = year;
+    this.speed = 0;
+  }
+  acceleration() {
+    this.speed += 10;
+  }
+  break() {
+    this.speed -= 5;
+  }
+  getStatus() {
+    return `${this.make} ${this.type} model is ${this.year} at travelling ${this.speed} km/h`;
+  }
+}
+
+const myCar = new Car("suzuki", "Maruti", 2010);
+myCar.acceleration();
+myCar.acceleration();
+myCar.break();
+console.log(myCar.getStatus());
